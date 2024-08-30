e9343079f471f74c84c3814b31943dc5d242bf9e
David <daviddwiyanto79@gmail.com>
2024-08-30 22:19:22 +0700

    feat(add footer)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Table from "@/components/Table";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col">
       <Navbar />
       <Table />
+      <Footer />
     </main>
   );
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
 
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
 const dummyData = [
     { id: 1, name: 'Item 1' },
     { id: 2, name: 'Item 2' },
@@ -11,21 +14,21 @@ const Table = () => {
   return (
     <div className="card bg-base-100 w-auto shadow-xl m-8">
     <div className="card-body">
-        <h2 className="card-title">List Perusahaan 0/22</h2>
+        <div className="inline-flex">
+            <h2 className="card-title">List Perusahaan</h2>
+            <div className="text-xl badge badge-ghost my-1">0/22</div>
+        </div>
         <div className="overflow-x-auto">   
         <table className="table">
             {/* head */}
             <thead>
             <tr>
-                <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
-                </th>
                 <th>No</th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Nama Perusahaan</th>
+                <th>Jenis</th>
+                <th>Lokasi Kota</th>
+                <th>Kemajuan</th>
+                <th>Status</th>
                 <th></th>
             </tr>
             </thead>
@@ -33,43 +36,53 @@ const Table = () => {
             {dummyData.map((p, index) => (
             <tbody key={index}>
                  <tr>
-                    <th>
-                    <label>
-                        <input type="checkbox" className="checkbox" />
-                    </label>
-                    </th>
                     <td>
                         {index + 1}
                     </td>
                     <td>
                     <div className="flex items-center gap-3">
-                        <div className="avatar">
-                        <div className="mask mask-squircle h-12 w-12">
-                        <Image
-                            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                            alt="Avatar Tailwind CSS Component"
-                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                            width={180}
-                            height={37}
-                            priority
-                            unoptimized
-                            />
-                        </div>
-                        </div>
-                        <div>
                         <div className="font-bold">Hart Hagerty</div>
-                        <div className="text-sm opacity-50">United States</div>
-                        </div>
                     </div>
                     </td>
                     <td>
-                    Zemlak, Daniel and Leannon
-                    <br />
-                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                    <Menu as="div" className="relative inline-block text-left">
+                    <div>
+                        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        Opsi
+                        <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
+                        </MenuButton>
+                    </div>
+
+                    <MenuItems
+                        transition
+                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                    >
+                        <div className="py-1">
+                        <MenuItem>
+                            <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                            >
+                            Studi Independen
+                            </a>
+                        </MenuItem>
+                        <MenuItem>
+                            <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                            >
+                            Magang
+                            </a>
+                        </MenuItem>
+                        </div>
+                    </MenuItems>
+                    </Menu>
                     </td>
                     <td>Purple</td>
+                    <td>Purple</td>
+                    <td>Purple</td>
                     <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
+                        <button className="btn btn-ghost btn-xs">details</button>
                     </th>
                 </tr>
             </tbody>
@@ -77,11 +90,12 @@ const Table = () => {
             {/* foot */}
             <tfoot>
             <tr>
-                <th></th>
                 <th>No</th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Nama Perusahaan</th>
+                <th>Jenis</th>
+                <th>Lokasi Kota</th>
+                <th>Kemajuan</th>
+                <th>Status</th>
                 <th></th>
             </tr>
             </tfoot>
