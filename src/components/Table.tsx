eb02f097429bacc5d27871eac0b9cbee9e28be68
David <daviddwiyanto79@gmail.com>
2024-08-31 14:47:48 +0700

    fix(delete pencil animation change into hero icon)

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
-import Pencil from './icons/Pencil'
 
 const dummyData = [
     { id: 1, name: 'Item 1' },
@@ -84,13 +83,19 @@ const Table = () => {
                     <td>
                         <div className='inline-flex'>
                             <p>Surabaya</p>
-                            <Pencil />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                            className="ml-3 hover:scale-150 hover:bg-gray-100 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                            </svg>
                         </div>
                     </td>
                     <td>
                         <div className='inline-flex'>
                             <p>Test TPA</p>
-                            <Pencil />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                            className="ml-3 hover:scale-150 hover:bg-gray-100 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                            </svg>
                         </div>
                     </td>
                     <td>
