565e40efffdb3189c48cf0ef73d5fd4ba521f26b
David <daviddwiyanto79@gmail.com>
2024-09-03 13:53:22 +0700

    fix(changing footer and finalizing table ui)

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,24 +2,29 @@
 
 import React, { useState } from 'react'
 
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-
 const dummyData = [
     { id: "1", name: 'Item 1' },
     { id: "2", name: 'Item 2' },
     { id: "3", name: 'Item 3' },
     { id: "4", name: 'Item 3' },
     { id: "5", name: 'Item 3' },
-    { id: "6", name: 'Item 3' },
-    { id: "7", name: 'Item 3' },
-    { id: "8", name: 'Item 3' },
-    { id: "9", name: 'Item 3' },
-    { id: "10", name: 'Item 3' },
-    { id: "11", name: 'Item 3' },
-    { id: "12", name: 'Item 3' },
-    { id: "13", name: 'Item 3' },
-    { id: "14", name: 'Item 3' },
+    // { id: "6", name: 'Item 3' },
+    // { id: "7", name: 'Item 3' },
+    // { id: "8", name: 'Item 3' },
+    // { id: "9", name: 'Item 3' },
+    // { id: "10", name: 'Item 3' },
+    // { id: "11", name: 'Item 3' },
+    // { id: "12", name: 'Item 3' },
+    // { id: "13", name: 'Item 3' },
+    // { id: "14", name: 'Item 3' },
+    // { id: "15", name: 'Item 3' },
+    // { id: "16", name: 'Item 3' },
+    // { id: "17", name: 'Item 3' },
+    // { id: "18", name: 'Item 3' },
+    // { id: "19", name: 'Item 3' },
+    // { id: "20", name: 'Item 3' },
+    // { id: "21", name: 'Item 3' },
+    // { id: "22", name: 'Item 3' },
 ]
 
 const Table = () => {
@@ -31,7 +36,7 @@ const Table = () => {
     setActiveColCity(activeColCity === id ? null : id);
   };
 
-//   // Column Progress
+  // Column Progress
   const [activeColProgress, setActiveColProgress] = useState<string | null>(null);
 
   const handleToggleColProgress = (id: string) => {
@@ -39,7 +44,11 @@ const Table = () => {
   };
 
   return (
-    <div className="card bg-base-100 w-auto shadow-xl m-8">
+    <div className="
+      md:card md:mx-5 md:my-6 
+      mx-0 my-0
+      bg-base-100 w-auto shadow-xl"
+      >
       <div className="card-body">
         <div className="inline-flex">
           <h2 className="card-title">List Perusahaan</h2>
@@ -60,144 +69,191 @@ const Table = () => {
               </tr>
             </thead>
             {/* body */}
-            {dummyData.map((p, index) => (
-              <tbody key={p.id}>
-                <tr>
-                  <td>{index + 1}</td>
-                  <td>
-                    <div className="flex items-center gap-3">
-                      <div className="font-bold">Hart Hagerty</div>
-                    </div>
-                  </td>
-                  <td>
-                    <Menu as="div" className="relative inline-block text-left">
-                      <div>
-                        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                          Opsi
-                          <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
-                        </MenuButton>
+            {dummyData.map((items, index) => {
+              const isItFirstRow = index === 0;
+              const isItLastRow = index === dummyData.length - 1;
+              const isItLastSecondRow = index === dummyData.length - 2;
+
+              return (
+              <>
+                <tbody key={items.id}>
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="font-bold">Hart Hagerty</div>
                       </div>
-                      <MenuItems
-                        transition
-                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none"
-                      >
-                        <div className="py-1">
-                          <MenuItem>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                              Studi Independen
-                            </a>
-                          </MenuItem>
-                          <MenuItem>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                              Magang
-                            </a>
-                          </MenuItem>
-                        </div>
-                      </MenuItems>
-                    </Menu>
-                  </td>
-                  <td>
-                    <div className='inline-flex'>
-                      {activeColCity === p.id ? (
-                        <input
-                          type="text"
-                          placeholder="Type here"
-                          className="input input-bordered hover:border-black w-full max-w-xs"
-                        />
+                    </td>
+                    <td>
+                    {isItFirstRow ? (
+                        <>
+                          <div className="dropdown">
+                              <div tabIndex={0} role="button" className="btn m-1">
+                                Opsi
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                                  <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                </svg>
+                              </div>
+                              <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                                <li><a>Studi Independen</a></li>
+                                <li><a>Magang</a></li>
+                              </ul>
+                            </div>  
+                        </>
+                      ) : isItLastRow || isItLastSecondRow ?(
+                        <>
+                          <div className="dropdown dropdown-right dropdown-end">
+                            <div tabIndex={0} role="button" className="btn m-1">
+                              Opsi
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                              </svg>
+                            </div>
+                            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                              <li><a>Studi Independen</a></li>
+                              <li><a>Magang</a></li>
+                            </ul>
+                          </div> 
+                        </>
                       ) : (
-                        <p>Surabaya</p>
+                        <>
+                          <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn m-1">
+                              Opsi
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                              </svg>
+                            </div>
+                            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                              <li><a>Studi Independen</a></li>
+                              <li><a>Magang</a></li>
+                            </ul>
+                          </div>  
+                        </>
                       )}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="ml-3 hover:scale-150 hover:bg-gray-100 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1"
-                        onClick={() => handleToggleColCity(p.id)} // Toggle visibility on click
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
-                        />
-                      </svg>
-                    </div>
-                  </td>
-                  <td>
-                    <div className='inline-flex'>
-                        {activeColProgress === p.id ? (
-                            <input
+                    </td>
+                    <td>
+                      <div className='inline-flex'>
+                        {activeColCity === items.id ? (
+                          <input
                             type="text"
                             placeholder="Type here"
                             className="input input-bordered hover:border-black w-full max-w-xs"
-                            />
+                          />
                         ) : (
-                            <p>Test TPA</p>
+                          <p>Surabaya</p>
                         )}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="ml-3 hover:scale-150 hover:bg-gray-100 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1"
-                        onClick={() => handleToggleColProgress(p.id)}
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
-                        />
-                      </svg>
-                    </div>
-                  </td>
-                  <td>
-                    <Menu as="div" className="relative inline-block text-left">
-                      <div>
-                        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                          Opsi
-                          <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
-                        </MenuButton>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="ml-3 hover:scale-150 hover:bg-gray-100 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1"
+                          onClick={() => handleToggleColCity(items.id)} // Toggle visibility on click
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
+                          />
+                        </svg>
                       </div>
-                      <MenuItems
-                        transition
-                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none"
-                      >
-                        <div className="py-1">
-                          <MenuItem>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                              Diterima
-                            </a>
-                          </MenuItem>
-                          <MenuItem>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                              Diproses
-                            </a>
-                          </MenuItem>
-                          <MenuItem>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                              Belum Lolos
-                            </a>
-                          </MenuItem>
-                          <MenuItem>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                              Ghosting ini, Fix!
-                            </a>
-                          </MenuItem>
+                    </td>
+                    <td>
+                      <div className='inline-flex'>
+                          {activeColProgress === items.id ? (
+                              <input
+                              type="text"
+                              placeholder="Type here"
+                              className="input input-bordered hover:border-black w-full max-w-xs"
+                              />
+                          ) : (
+                              <p>Test TPA</p>
+                          )}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="ml-3 hover:scale-150 hover:bg-gray-100 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1"
+                          onClick={() => handleToggleColProgress(items.id)}
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
+                          />
+                        </svg>
+                      </div>
+                    </td>
+                    <td>
+                    {isItFirstRow ? (
+                      <>
+                        <div className="dropdown">
+                          <div tabIndex={0} role="button" className="btn m-1">
+                            Opsi
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                            </svg>
+                          </div>
+                          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                            <li><a>Diterima</a></li>
+                            <li><a>Diproses</a></li>
+                            <li><a>Terdaftar</a></li>
+                            <li><a>Tidak Lolos</a></li>
+                          </ul>
                         </div>
-                      </MenuItems>
-                    </Menu>
-                  </td>
-                  <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
-                  </th>
-                </tr>
-              </tbody>
-            ))}
+                      </>
+                    ) : isItLastRow || isItLastSecondRow ? (
+                      <>
+                        <div className="dropdown dropdown-left dropdown-end">
+                          <div tabIndex={0} role="button" className="btn m-1">
+                            Opsi
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                              </svg>
+                          </div>
+                          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                              <li><a>Diterima</a></li>
+                              <li><a>Diproses</a></li>
+                              <li><a>Terdaftar</a></li>
+                              <li><a>Tidak Lolos</a></li>
+                          </ul>
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <div className="dropdown">
+                          <div tabIndex={0} role="button" className="btn m-1">
+                            Opsi
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                            </svg>
+                          </div>
+                          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                            <li><a>Diterima</a></li>
+                            <li><a>Diproses</a></li>
+                            <li><a>Terdaftar</a></li>
+                            <li><a>Tidak Lolos</a></li>
+                          </ul>
+                        </div>
+                      </>
+                    )}
+                    
+                    </td>
+                    <th>
+                      <button className="btn btn-ghost btn-xs">detail</button>
+                    </th>
+                  </tr>
+                </tbody>
+              </>
+              )
+            })}
             {/* foot */}
-            <tfoot>
-              <tr>
+            <tfoot className={`${dummyData.length >= 3 ? '' : 'h-56'}`}>
+              <tr className={`${dummyData.length >= 3 ? '' : 'hidden'}`}>
                 <th>No</th>
                 <th>Nama Perusahaan</th>
                 <th>Jenis</th>
