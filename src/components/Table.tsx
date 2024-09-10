7f0cfce63824c7901188abc25b6bd0dd55cc350b
David <daviddwiyanto79@gmail.com>
2024-09-11 02:51:17 +0700

    fix(footer, table UI)

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,7 +8,7 @@ const dummyData = [
     { id: "3", name: 'Item 3' },
     { id: "4", name: 'Item 3' },
     { id: "5", name: 'Item 3' },
-    // { id: "6", name: 'Item 3' },
+    { id: "6", name: 'Item 3' },
     // { id: "7", name: 'Item 3' },
     // { id: "8", name: 'Item 3' },
     // { id: "9", name: 'Item 3' },
@@ -30,7 +30,14 @@ const dummyData = [
 const Table = () => {
   
   // Column City
-  const [activeColCity, setActiveColCity] = useState<string | null>(null);
+  const [activeColCompany, setActiveColCompany] = useState<string | null>(null);
+
+  const handleToggleColCompany = (id: string) => {
+    setActiveColCompany(activeColCompany === id ? null : id);
+  };
+
+  // Column City
+  const [activeColCity, setActiveColCity] = useState<string | null>(null);``
 
   const handleToggleColCity = (id: string) => {
     setActiveColCity(activeColCity === id ? null : id);
@@ -77,18 +84,54 @@ const Table = () => {
               return (
               <>
                 <tbody key={items.id}>
-                  <tr>
+                  <tr className='hover:bg-gray-100/65'>
                     <td>{index + 1}</td>
                     <td>
                       <div className="flex items-center gap-3">
-                        <div className="font-bold">Hart Hagerty</div>
+                        {activeColCompany === items.id ? (
+                          <input
+                            type="text"
+                            placeholder="Type here"
+                            className="input input-bordered hover:border-black w-full max-w-xs"
+                          />
+                        ) : (
+                          <div className="font-bold">Hart Hagerty</div>
+                        )}
+                        {activeColCompany === items.id ? (
+                           <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" viewBox="0 0 24 24" 
+                            strokeWidth={1.5} 
+                            stroke="green"
+                            className="scale-125 hover:scale-150 hover:bg-gray-200 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1"
+                            onClick={() => handleToggleColCompany(items.id)}
+                          >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                          </svg>
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="hover:scale-150 hover:bg-gray-200 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-2"
+                            onClick={() => handleToggleColCompany(items.id)}
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
+                            />
+                          </svg>
+                        )}
                       </div>
                     </td>
                     <td>
                     {isItFirstRow ? (
                         <>
                           <div className="dropdown">
-                              <div tabIndex={0} role="button" className="btn m-1">
+                              <div tabIndex={0} role="button" className="btn">
                                 Opsi
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                   <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
@@ -103,7 +146,7 @@ const Table = () => {
                       ) : isItLastRow || isItLastSecondRow ?(
                         <>
                           <div className="dropdown dropdown-right dropdown-end">
-                            <div tabIndex={0} role="button" className="btn m-1">
+                            <div tabIndex={0} role="button" className="btn">
                               Opsi
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
@@ -118,7 +161,7 @@ const Table = () => {
                       ) : (
                         <>
                           <div className="dropdown">
-                            <div tabIndex={0} role="button" className="btn m-1">
+                            <div tabIndex={0} role="button" className="btn">
                               Opsi
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
@@ -143,21 +186,34 @@ const Table = () => {
                         ) : (
                           <p>Surabaya</p>
                         )}
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="ml-3 hover:scale-150 hover:bg-gray-100 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1"
-                          onClick={() => handleToggleColCity(items.id)} // Toggle visibility on click
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
-                          />
-                        </svg>
+                        {activeColCity === items.id ? (
+                           <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" viewBox="0 0 24 24" 
+                            strokeWidth={1.5} 
+                            stroke="green"
+                            className="ml-3 scale-125 hover:scale-150 hover:bg-gray-200 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 mt-1.5"
+                            onClick={() => handleToggleColCity(items.id)}
+                          >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                          </svg>
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="ml-3 hover:scale-150 hover:bg-gray-200 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-2"
+                            onClick={() => handleToggleColCity(items.id)}
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
+                            />
+                          </svg>
+                        )}
                       </div>
                     </td>
                     <td>
@@ -171,28 +227,41 @@ const Table = () => {
                           ) : (
                               <p>Test TPA</p>
                           )}
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="ml-3 hover:scale-150 hover:bg-gray-100 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1"
-                          onClick={() => handleToggleColProgress(items.id)}
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
-                          />
-                        </svg>
+                          {activeColProgress === items.id ? (
+                              <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill="none" viewBox="0 0 24 24" 
+                                strokeWidth={1.5} 
+                                stroke="green"
+                                className="ml-3 scale-125 hover:scale-150 hover:bg-gray-200 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 mt-1.5"
+                                onClick={() => handleToggleColProgress(items.id)}
+                              >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                              </svg>
+                          ) : (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="ml-3 hover:scale-150 hover:bg-gray-200 p-1 hover:rounded-md hover:cursor-pointer duration-150 size-7 -mt-1"
+                              onClick={() => handleToggleColProgress(items.id)}
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487z"
+                              />
+                            </svg>
+                          )}
                       </div>
                     </td>
                     <td>
                     {isItFirstRow ? (
                       <>
                         <div className="dropdown">
-                          <div tabIndex={0} role="button" className="btn m-1">
+                          <div tabIndex={0} role="button" className="btn">
                             Opsi
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                               <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
@@ -209,7 +278,7 @@ const Table = () => {
                     ) : isItLastRow || isItLastSecondRow ? (
                       <>
                         <div className="dropdown dropdown-left dropdown-end">
-                          <div tabIndex={0} role="button" className="btn m-1">
+                          <div tabIndex={0} role="button" className="btn">
                             Opsi
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
@@ -226,7 +295,7 @@ const Table = () => {
                     ) : (
                       <>
                         <div className="dropdown">
-                          <div tabIndex={0} role="button" className="btn m-1">
+                          <div tabIndex={0} role="button" className="btn">
                             Opsi
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                               <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
@@ -244,7 +313,19 @@ const Table = () => {
                     
                     </td>
                     <th>
-                      <button className="btn btn-ghost btn-xs">detail</button>
+                      <div className='inline-flex'>
+                        <button className="btn btn-ghost btn-xs">detail</button>
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg" 
+                          fill="none" 
+                          viewBox="0 0 24 24" 
+                          strokeWidth={1.5} 
+                          stroke="red" 
+                          className="scale-110 hover:scale-100 hover:bg-gray-200 hover:rounded-md hover:cursor-pointer duration-150 size-6 p-1"
+                        >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                      </div>
                     </th>
                   </tr>
                 </tbody>
